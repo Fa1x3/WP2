ea244c52566ac0f23caba112b8382863f72b43ac
Alex <91316417+delllc@users.noreply.github.com>
2022-06-13 19:45:19 +0500

    артур эмобой

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-
+$(document).ready(function(){
+    $('a[href^="#"], *[data-href^="#"]').on('click', function(e){
+        e.preventDefault();
+        var t = 1000;
+        var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+        $('html,body').stop().animate({ scrollTop: $(d).offset().top }, t);
+    });
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 1,
+        autoplayTimeout:2000,
+        autoplay: true,
+        dots: true
+    });
+});
